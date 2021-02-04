0252bf895d49ad07ef163d59d4d52bdc0a9f1bf7
Onur Cantay <onur.cantay@accenture.com>
2021-02-04 14:59:17 +0100

    minor changes

--- startup_namer/startup_namer/lib/main.dart
+++ startup_namer/startup_namer/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main(List<String> args) {
   runApp(StartupNamer());
@@ -7,14 +8,15 @@ void main(List<String> args) {
 class StartupNamer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
