98a9e647463de9905f2804619b6b2237b6edff04
Onur Cantay <onur.cantay@accenture.com>
2021-02-04 14:13:57 +0100

    Initial Commit for Startup Namer

---
+++ startup_namer/startup_namer/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(StartupNamer());
+}
+
+class StartupNamer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: const Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
