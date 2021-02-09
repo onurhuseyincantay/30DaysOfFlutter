54e51283b170ef408d1cd745e229c2b72a12c342
Onur Cantay <onur.cantay@accenture.com>
2021-02-09 11:58:52 +0100

    added listview seperated states imported external dart file

--- startup_namer/startup_namer/lib/main.dart
+++ startup_namer/startup_namer/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'random_words.dart';
 
 void main(List<String> args) {
   runApp(StartupNamer());
@@ -8,17 +8,9 @@ void main(List<String> args) {
 class StartupNamer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
