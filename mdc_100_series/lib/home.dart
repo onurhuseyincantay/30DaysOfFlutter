894724588d1a6492f53688339cf886f55d4269a2
Onur Cantay <onur.cantay@accenture.com>
2021-02-10 13:11:51 +0100

    Finalized MDC-102

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -13,6 +13,9 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'model/products_repository.dart';
+import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
   // TODO: Make a collection of cards (102)
@@ -22,12 +25,97 @@ class HomePage extends StatelessWidget {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
-      // TODO: Add app bar (102)
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: "menu",
+          ),
+          onPressed: _pressedMenuButton,
+        ),
+        title: Text("Shrine"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Icons.search,
+                semanticLabel: "Search",
+              ),
+              onPressed: _searchPressed),
+          IconButton(
+              icon: Icon(
+                Icons.tune,
+                semanticLabel: "Filter",
+              ),
+              onPressed: _filterPressed),
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(2),
+        childAspectRatio: 8.0 / 9.0,
+        children: _buildGridCards(context),
       ),
       resizeToAvoidBottomInset: false,
     );
+  }
+
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+    List<Card> cards = products
+        .map((product) => Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 18.0 / 11.0,
+                    child: Image.asset(
+                      product.assetName,
+                      package: product.assetPackage,
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16, 12, 16, 8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          product.name,
+                          style: theme.textTheme.headline6,
+                          maxLines: 1,
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          formatter.format(product.price),
+                          style: theme.textTheme.subtitle2,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ))
+        .toList();
+    return cards;
+  }
+
+  void _pressedMenuButton() {
+    print("Menu Pressed");
+  }
+
+  void _searchPressed() {
+    print("Search Pressed");
+  }
+
+  void _filterPressed() {
+    print("Filter Pressed");
   }
 }
